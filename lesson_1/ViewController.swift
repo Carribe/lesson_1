ad6c783bd2f30f207208a3ed9c291b2768d586ae
Artemii <iktm181@gmail.com>
2023-09-20 22:46:51 +0300

    Initial Commit

---
+++ lesson_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson_1
+//
+//  Created by Артём on 20.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
